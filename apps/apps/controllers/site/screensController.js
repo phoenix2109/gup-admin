2f609cb3d2df6728c5b5fbfb9c89614efe9b93b3
Phoenix <ha.doanthanh.hust@gmail.com>
2020-01-02 11:08:27 +0700

    Get started

---
+++ apps/apps/controllers/site/screensController.js
@@ -0,0 +1,36 @@
+// const productModel = require('../../models/site')
+const moment = require('moment')
+const siteModel = require('../../models/site/siteModel')
+
+module.exports = {
+  register: async (req, res) => {
+    res.render('site/screens/register', {
+      data: {
+
+      }
+    })
+  },
+  registerPost: async (req, res) => {
+    const { account, name, phone, email } = req.body
+    // console.log(req.body)
+    const data = {
+      account: account,
+      name: name,
+      phone: phone,
+      email: email,
+      status: 0,
+      createAt: new Date(moment(new Date()).utcOffset(420).format('YYYY-MM-DD HH:mm')).toISOString(),
+      modifyAt: ''
+    }
+    // console.log(data)
+    siteModel.addCustomer(_config('database.collection.customer'), data)
+    res.redirect('/')
+  },
+  helpcenter: async (req, res) => {
+    res.render('site/screens/register', {
+      data: {
+
+      }
+    })
+  }
+}
